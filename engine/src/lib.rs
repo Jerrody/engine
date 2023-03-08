39ea77100f69b44f4d45a7ae45285acadd2c9c2e
George <huntermlg123465@gmail.com>
2023-03-08 23:26:17 +0200

    Terminated nested complexity in engine folders and deleted orphan folders.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,6 +1,19 @@
 #![feature(panic_info_message)]
 #![deny(unsafe_op_in_unsafe_fn)]
 
-mod engine;
+mod logging;
+mod renderer;
 
-pub use engine::*;
+pub struct Engine {
+    renderer: renderer::Renderer,
+    logging: logging::Logging,
+}
+
+impl Engine {
+    pub fn new(window: &winit::window::Window) -> Self {
+        let renderer = renderer::Renderer::new(window);
+        let logging = logging::Logging::new();
+
+        Self { renderer, logging }
+    }
+}
